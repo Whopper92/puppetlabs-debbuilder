0b1353f7b4fc2c7ad7e7a944f77b03369f5d2684
Matthaus Litteken <matthaus@puppetlabs.com>
2012-08-15 23:46:10 -0700

    Stuff

--- manifests/setup/cow_exec.pp
+++ manifests/setup/cow_exec.pp
@@ -4,7 +4,7 @@ define debbuilder::setup::cow_exec (
 
   exec { "${name}-i386":
     path          => "/usr/sbin:/usr/bin:/bin:/sbin",
-    command       => "cowbuilder --create --dist ${name} --architecture i386 --debug",
+    command       => "cowbuilder --create --debug",
     unless        => "test -e ${cow_root}/base-${name}-i386.cow",
     environment   => ["DIST=${name}", "ARCH=i386"],
     logoutput     => on_failure,
@@ -13,11 +13,29 @@ define debbuilder::setup::cow_exec (
 
   exec { "${name}-amd64":
     path          => "/usr/sbin:/usr/bin:/bin:/sbin",
-    command       => "cowbuilder --create --dist ${name} --architecture amd64 --debug",
+    command       => "cowbuilder --create --debug",
     unless        => "test -e ${cow_root}/base-${name}-amd64.cow",
     environment   => ["DIST=${name}", "ARCH=amd64"],
     logoutput     => on_failure,
     timeout       => 0,
+  }
+
+  cron { "${name}-i386":
+    command       => "cowbuilder --update",
+    environment   => "DIST=${name} ARCH=i386 PATH=/usr/sbin:/usr/bin:/bin:/sbin",
+    hour          => "2",
+    minute        => "15",
+    name          => "cowbuilder update for ${name}-i386",
+    user          => root,
+  }
+
+  cron { "${name}-amd64":
+    command       => "cowbuilder --update",
+    environment   => "DIST=${name} ARCH=amd64 PATH=/usr/sbin:/usr/bin:/bin:/sbin",
+    hour          => "2",
+    minute        => "15",
+    name          => "cowbuilder update for ${name}-amd64",
+    user          => root,
   }
 
 }
